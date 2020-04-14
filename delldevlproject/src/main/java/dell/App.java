11221951c35dc46337a214473d7433b7440feaca
root <root@caronaVM.qwfunwwfkdeunajjy21omnmfjd.bx.internal.cloudapp.net>
2020-04-14 05:48:44 +0000

    projects

---
+++ delldevlproject/src/main/java/dell/App.java
@@ -0,0 +1,16 @@
+package dell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+class C {
+}
+class D {} 
+   public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
